15c629b75504731500772659a31e42767bfc1e95
furusawa-dhu <furusawa.d-hu@nhk.or.jp>
2024-11-27 17:41:39 +0900

    ActorCardの改修

--- frontend/src/components/ActorModalCard.js
+++ frontend/src/components/ActorModalCard.js
@@ -5,48 +5,118 @@ import {
   ScrollableList,
   Modal,
   ActorInfo,
+  NPlaylistLargeImageCard,
+  Text,
 } from "@d7lab/bx-components-react";
 
 export const ActorModalCard = () => {
   // どのAPIのレスポンスを持っているか、Modalを表示するかなどを管理するためにuseStateでstateを作成します。
-  const [response, setResponse] = useState(); // コンポーネントが受け取ったAPIのレスポンスを管理するstate
+  const [nplaylistInfo, setNPlaylistInfo] = useState(); // コンポーネントが受け取ったAPIのレスポンスを管理するstate
+  const [extendedByNpgId, setExtendedByNpgId] = useState([]); // npgIdで検索した結果を管理する
   const [openModal, setOpenModal] = useState(false); // Modalを表示するか否かを管理するstate
-  const [selectedActorIndex, setSelectedActorIndex] = useState(null); // どこのActorCardがclickされたかを管理するstate
+  const [selectedActorId, setSelectedActorId] = useState(null); // どこのActorCardがclickされたかを管理するstate
 
   // APIを取得する関数
-  const fetchAPI = async (url) => {
+  const fetchAPI = async (url, callback) => {
     try {
       const res = await fetch(url);
       const data = await res.json();
-      setResponse(data);
-      console.log(response);
+      callback(data);
+      console.log(data);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
 
-  // "APIを取得する関数"の実行
+  // 検索API用の関数
+  const fetchExtendedAPI = async (url, callback, list = null, query) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      callback((prevList) => [...prevList, { id: query, data: data }]);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  // APIを取得する関数の実行
   useEffect(() => {
     fetchAPI(
-      "https://api.nr.nhk.jp/r7/t/nplaylist/pl/series-tep-R7Y6NGLJ6G.json"
+      "https://api.nr.nhk.jp/r7/t/nplaylist/pl/series-tep-R7Y6NGLJ6G.json",
+      setNPlaylistInfo
     );
   }, []);
+
+  // APIの結果からnpgIdを取得し、追加で検索APIを叩く関数の実行
+  useEffect(() => {
+    if (nplaylistInfo) {
+      nplaylistInfo.actor.forEach((e) => {
+        if (e.person) {
+          fetchExtendedAPI(
+            `https://api.nr.nhk.jp/r7/s/extended2.json?modeOfItem=tv&type=nplaylist&npgId=${e.person.identifierGroup.npgId}`,
+            setExtendedByNpgId,
+            extendedByNpgId,
+            e.person.identifierGroup.npgId
+          );
+        } else if (e.organization) {
+          fetchExtendedAPI(
+            `https://api.nr.nhk.jp/r7/s/extended2.json?modeOfItem=tv&type=nplaylist&npgId=${e.organization.identifierGroup.npgId}`,
+            setExtendedByNpgId,
+            extendedByNpgId,
+            e.person.identifierGroup.npgId
+          );
+        }
+      });
+    }
+  }, [nplaylistInfo]);
+
+  // idを指定してactor[]からpersonまたはorganizationを取得する関数
+  function getActorDataById(actorArray, targetId) {
+    for (const actorItem of actorArray) {
+      if (
+        actorItem.person &&
+        actorItem.person.identifierGroup.npgId === targetId
+      ) {
+        return actorItem;
+      }
+      if (
+        actorItem.organization &&
+        actorItem.organization.identifierGroup.npgId === targetId
+      ) {
+        return actorItem.organization;
+      }
+    }
+    return null;
+  }
+
+  // extendedByNpgIdから対象のidを取得する関数
+  function getExtendedDataById(listOfObjects, id) {
+    const foundObject = listOfObjects.find((obj) => obj.id === id);
+    return foundObject ? foundObject.data : null;
+  }
 
   // 画面上に表示するActorCardのリスト
   return (
     <>
-      {response ? (
+      {nplaylistInfo ? (
         <>
           <BaseLayout>
             <ScrollableList>
-              {response.actor.map((e, index) => (
+              {nplaylistInfo.actor.map((e, index) => (
                 <>
                   <ActorCard
                     key={index}
                     content={e}
                     customStyles={{ width: 200, m: "A" }}
                     onClick={() => {
-                      setSelectedActorIndex(index);
+                      if (e.person) {
+                        setSelectedActorId(e.person.identifierGroup.npgId);
+                      } else if (e.organization) {
+                        setSelectedActorId(
+                          e.organization.identifierGroup.npgId
+                        );
+                      }
                       setOpenModal(true);
                     }}
                   />
@@ -58,9 +128,29 @@ export const ActorModalCard = () => {
       ) : (
         <p>データを取得できませんでした</p>
       )}
-      {selectedActorIndex !== null && (
+      {selectedActorId !== null && (
         <Modal open={openModal} onClose={() => setOpenModal(false)}>
-          <ActorInfo content={response.actor[selectedActorIndex]} />
+          <ActorInfo
+            content={getActorDataById(nplaylistInfo.actor, selectedActorId)}
+          />
+          <BaseLayout>
+            <Text align="left" tag="h2" whiteSpace={{}}>
+              出演シリーズ
+            </Text>
+            <ScrollableList>
+              {getExtendedDataById(
+                extendedByNpgId,
+                selectedActorId
+              )?.result.nplaylist.result.map((e, index) => (
+                <NPlaylistLargeImageCard
+                  ke={index}
+                  content={e}
+                  customStyles={{ width: 200, m: "A" }}
+                  showPlaylistName
+                />
+              ))}
+            </ScrollableList>
+          </BaseLayout>
         </Modal>
       )}
     </>
